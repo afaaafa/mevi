5fca64a93fc3bac8d996fd4021228b8dd27e07fe
AF <af.arthurfaria@gmail.com>
2026-03-12 17:45:17 -0300

    feat: crud for patients

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,70 +1,50 @@
 class PatientsController < ApplicationController
   before_action :set_patient, only: %i[ show edit update destroy ]
 
-  # GET /patients or /patients.json
   def index
-    @patients = Patient.all
+    @patients = Current.user.patients
   end
 
-  # GET /patients/1 or /patients/1.json
   def show
   end
 
-  # GET /patients/new
   def new
     @patient = Patient.new
   end
 
-  # GET /patients/1/edit
   def edit
   end
 
-  # POST /patients or /patients.json
   def create
-    @patient = Patient.new(patient_params)
+    @patient = Current.user.patients.build(patient_params)
 
-    respond_to do |format|
-      if @patient.save
-        format.html { redirect_to @patient, notice: "Patient was successfully created." }
-        format.json { render :show, status: :created, location: @patient }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @patient.errors, status: :unprocessable_entity }
-      end
+    if @patient.save
+      redirect_to @patient, notice: "Paciente cadastrado com sucesso."
+    else
+      render :new, status: :unprocessable_entity
     end
   end
 
-  # PATCH/PUT /patients/1 or /patients/1.json
   def update
-    respond_to do |format|
-      if @patient.update(patient_params)
-        format.html { redirect_to @patient, notice: "Patient was successfully updated.", status: :see_other }
-        format.json { render :show, status: :ok, location: @patient }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @patient.errors, status: :unprocessable_entity }
-      end
+    if @patient.update(patient_params)
+      redirect_to @patient, notice: "Paciente atualizado com sucesso.", status: :see_other
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
-  # DELETE /patients/1 or /patients/1.json
   def destroy
     @patient.destroy!
-
-    respond_to do |format|
-      format.html { redirect_to patients_path, notice: "Patient was successfully destroyed.", status: :see_other }
-      format.json { head :no_content }
-    end
+    redirect_to patients_path, notice: "Paciente removido.", status: :see_other
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_patient
-      @patient = Patient.find(params.expect(:id))
-    end
 
-    # Only allow a list of trusted parameters through.
-    def patient_params
-      params.expect(patient: [ :user_id, :name, :birth_date, :email, :phone, :description ])
-    end
+  def set_patient
+    @patient = Current.user.patients.find(params[:id])
+  end
+
+  def patient_params
+    params.expect(patient: [ :name, :birth_date, :email, :phone, :description ])
+  end
 end
